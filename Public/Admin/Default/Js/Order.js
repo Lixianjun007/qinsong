c9a60aaffdf9a49bbd1a20529670469759baae19
lxj <602261903@qq.com>
2018-10-28 13:26:20 +0800

    还原

--- Public/Admin/Default/Js/Order.js
+++ Public/Admin/Default/Js/Order.js
@@ -1,29 +1,29 @@
-$(function ()
+$(function()
 {
     // 颜色选择器
     var $input_tag = $('input[name="title"]');
     var $title_color = $('input[name="title_color"]');
     $(".colorpicker-submit").colorpicker(
-            {
-                fillcolor: true,
-                success: function (o, color)
-                {
-                    $input_tag.css('color', color);
-                    $title_color.val(color);
-                },
-                reset: function (o)
-                {
-                    $input_tag.css('color', '');
-                    $title_color.val('');
-                }
-            });
+    {
+        fillcolor:true,
+        success:function(o, color)
+        {
+            $input_tag.css('color', color);
+            $title_color.val(color);
+        },
+        reset:function(o)
+        {
+            $input_tag.css('color', '');
+            $title_color.val('');
+        }
+    });
 
     /* 搜索切换 */
     var $more_where = $('.more-where');
     $more_submit = $('.more-submit');
-    $more_submit.find('input[name="is_more"]').change(function ()
+    $more_submit.find('input[name="is_more"]').change(function()
     {
-        if ($more_submit.find('i').hasClass('am-icon-angle-down'))
+        if($more_submit.find('i').hasClass('am-icon-angle-down'))
         {
             $more_submit.find('i').removeClass('am-icon-angle-down');
             $more_submit.find('i').addClass('am-icon-angle-up');
@@ -31,8 +31,8 @@ $(function ()
             $more_submit.find('i').addClass('am-icon-angle-down');
             $more_submit.find('i').removeClass('am-icon-angle-up');
         }
-
-        if ($more_submit.find('input[name="is_more"]:checked').val() == undefined)
+    
+        if($more_submit.find('input[name="is_more"]:checked').val() == undefined)
         {
             $more_where.addClass('none');
         } else {
@@ -46,20 +46,20 @@ $(function ()
     var nowTemp = new Date();
     var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
 
-    var checkin = $time_start.datepicker({}).on('changeDate.datepicker.amui', function (ev) {
+    var checkin = $time_start.datepicker({}).on('changeDate.datepicker.amui', function(ev) {
         var newDate = new Date(ev.date)
         newDate = (ev.date.valueOf() > checkout.date.valueOf() || ev.date.valueOf() == checkout.date.valueOf()) ? newDate.setDate(newDate.getDate() + 1) : checkout.date.valueOf();
         checkout.setValue(newDate);
-
+        
         checkin.close();
         $time_end[0].blur();
     }).data('amui.datepicker');
     var checkout = $time_end.datepicker({
-        onRender: function (date) {
+        onRender:function(date) {
             return date.valueOf() <= checkin.date.valueOf() ? 'am-disabled' : '';
         }
-    }).on('changeDate.datepicker.amui', function (ev) {
-        checkout.close();
+    }).on('changeDate.datepicker.amui', function(ev) {
+      checkout.close();
     }).data('amui.datepicker');
 
 
@@ -119,7 +119,7 @@ $(function ()
 
             },
             onCancel: function () {}
-        });
+});
     }
     );
 
